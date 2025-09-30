9c7afa7fd7a661110d9a2903c59f4110ef44ed72
vivek Singh <vek.sgh@gmail.com>
2025-09-30 23:11:32 +0530

    upading code

--- index.js
+++ index.js
@@ -37,3 +37,21 @@
 // console.log(c)  // --Error (cannot reassign)
 
 
+
+//--------------------------
+var x= 1;
+a();
+b();
+console.log(x);
+
+
+function a(){
+    var x = 10;
+    console.log(x);
+}
+
+
+function b(){
+    var x = 100;
+    console.log(x)
+}
