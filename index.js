1cd07a519ac175b21b0c1baccb74079006496f0b
vivek Singh <vek.sgh@gmail.com>
2025-09-30 23:06:20 +0530

    hoisting and variables

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-var x  = 7;
+//-----hoisting-------------------------------
+
+// getName();
+// console.log(x)
 
 
-function getName(){
-    console.log("hello world")
-}
-getName();
-console.log(x)
+// var x  = 7;
+
+// function getName(){
+//     console.log("hello world")
+// }
+
+
+// var getName2 = function (){
+//     console.log("hello world")
+// }
+
+
+
+// var getName = () =>{
+//     console.log("hello world")
+// }
+
+
+
+//--------------------variables-----------------
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// a = 15; 
+// b = 25; 
+// c = 35;
+
+// console.log(a)
+// console.log(b)
+// console.log(c)  // --Error (cannot reassign)
+
+
